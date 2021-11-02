d011e575bc00eca998b8e4b34196185dd5b70c8e
Daniel <danielborjas.92@gmail.com>
2021-11-02 17:34:23 -0600

    docs agregados

---
+++ docs/precache-manifest.2084a2db1cf6f83913f143ed018be406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad32f020cada4ca58f6e04effb6a3e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477af74f6d5759d8e64e",
+    "url": "/static/js/main.80d66a61.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
